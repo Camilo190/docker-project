a4028a4abdcce57e976934ff308e9d6f74d4eac1
Camilo Ciro Orozco <camilo_ciro@hotmail.com>
2023-07-13 20:47:51 -0500

    create backend submodule

--- springboot-app/src/main/java/com/camilo/myspringproject/repository/ITestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.camilo.myspringproject.repository;
-
-import com.camilo.myspringproject.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ITestRepository extends JpaRepository<Test, Long> {
-}
